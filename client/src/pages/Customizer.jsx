45c6bffd7afe054a62db9b3c9fd1142f2cbe5bed
malik ibo <malik97un@gmail.com>
2025-02-16 02:03:11 +0100

    Ajout de la config Netlify

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -58,7 +58,7 @@ const Customizer = () => {
       setGeneratingImg(true);
 
       console.log("Prompt envoyé au backend :", prompt);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://t-shirt-3d.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
